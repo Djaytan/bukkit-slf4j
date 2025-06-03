b83b9481e1f4e5af3a90647b96430d27baa23b53
Djaytan <26904516+Djaytan@users.noreply.github.com>
2025-06-03 21:20:33 +0200

    ci: adjust commitlint config to new conventional commits guidelines (#215)

--- .github/release/commitlint.config.js
+++ .github/release/commitlint.config.js
@@ -2,16 +2,10 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     // Project specific rules
-    'scope-enum': [2, 'always', [
-      'readme', 'contributing', 'packaging', 'deps', 'other', 'github',
-      'renovate', 'release'
-    ]],
+    'scope-enum': [2, 'always', ['deps']],
 
     // Compliant with Angular conventions (get rid of "style" type but tolerate "chore" one)
-    'type-enum': [2, 'always', [
-      'feat', 'fix', 'perf', 'revert', 'refactor', 'build', 'test', 'ci',
-      'docs', 'chore'
-    ]],
+    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'revert', 'refactor', 'build', 'test', 'ci', 'docs', 'chore']],
 
     // Adjust some rules predefined by @commitlint/config-conventional
     'body-max-line-length': [0, 'always', 'Infinity'],
